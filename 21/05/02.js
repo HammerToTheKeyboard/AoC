44bb3ace868e9c3e20cb98459dc302480523cbdd
HammerToTheKeyboard <j.d.weisepost@gmail.com>
2021-12-05 18:22:05 +0100

    day 5 done

--- 21/05/02.js
+++ 21/05/02.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require("path");
-const rawInput = fs.readFileSync(path.resolve(__dirname,'input2')).toString()
+const rawInput = fs.readFileSync(path.resolve(__dirname,'input')).toString()
 
 const input = rawInput.split(/\D/g).filter(e => e).map(e => parseInt(e))
 const largestNum = Math.max.apply(null, input)
@@ -48,17 +48,45 @@ function mapHorizontal(x1,y1,x2,y2){
     }
 }
 
+function mapDiagonal(x1,y1,x2,y2){
+    let a
+    let b
+    if (x1 < x2) {
+        a = 1
+        if (y1 < y2) {
+            b = 1
+        } else {
+            b = -1
+        }
+    } else {
+        a = -1
+        if (y1 < y2) {
+            b = 1 
+        } else {
+            b = -1
+        }
+    }
+    let x = x1
+    let y = y1
+    for(let i = 0; i <= Math.abs(x2 - x1); i++){
+        map[y][x]++
+        y += b
+        x += a
+    } 
+
+}
+
 function determineDirection(){
     for(let i = 0; i < input.length; i +=4){
         if(input[i] == input[i+2]) mapVertical(input[i],input[i+1],input[i+2],input[i+3])
         else if (input[i+1] == input[i+3]) mapHorizontal(input[i],input[1+i],input[2+i],input[3+i])
+        else mapDiagonal(input[i],input[i+1],input[i+2],input[i+3]) 
     }
 }
 
 function evaluateMap(){
     determineDirection()
     data = map.map(e => e.toString().replace(/0/g,".")+"\n").toString().replace(/,/g,"")
-    console.log(data)
     const hotspots = map.reduce((a,b) => a.concat(b)).filter(e => e >= 2)
     
     return hotspots.length
@@ -67,16 +95,16 @@ function evaluateMap(){
 const answer = evaluateMap()
 console.log(answer)
 
-//Solved. Answer = 8350
+//Solved. Answer = 19374
 
 
   
-// fs.writeFile("ventsp2.txt", data, (err) => {
+// fs.writeFile("vents2p2.txt", data, (err) => {
 //   if (err)
 //     console.log(err);
 //   else {
 //     console.log("File written successfully\n")
 //     console.log("The written has the following contents:")
-//     console.log(fs.readFileSync("ventsp2.txt", "utf8"))
+//     console.log(fs.readFileSync("vents2p2.txt", "utf8"))
 //   }
 // })
