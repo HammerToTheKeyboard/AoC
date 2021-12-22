c7e65b587b6b78e2b57680a8b1722ee698a14f42
HammerToTheKeyboard <j.d.weisepost@gmail.com>
2021-12-22 20:56:04 +0100

    i achieved literally nothing

--- 21/22/01.js
+++ 21/22/01.js
@@ -8,34 +8,65 @@ let activeCubes = new Set()
 let o = 0
 
 function makeCommand(line) {
-    let command = {mode: line[0], x1: parseInt(line[1]), x2: parseInt(line[2]), y1: parseInt(line[3]), y2: parseInt(line[4]), z1: parseInt(line[5]), z2: parseInt(line[6])}
-    return command
+    let cmd = {mode: line[0], x1: parseInt(line[1]), x2: parseInt(line[2]), y1: parseInt(line[3]), y2: parseInt(line[4]), z1: parseInt(line[5]), z2: parseInt(line[6])}
+    return cmd
 }
 
-function executeCommand(cmd) {
-    for(let x = cmd.x1; x <= cmd.x2; x++){
-        for(let y = cmd.y1; y <= cmd.y2; y++){
-            for(let z = cmd.z1; z <= cmd.z2; z++){
-                if(cmd.mode == "off"){
-                    if(activeCubes.delete(`${x}${y}${z}`)) o++
-                } 
-                else if(cmd.mode == "on"){
-                    activeCubes.add(`${x}${y}${z}`)                    
-                } 
-            }
-        }
+function crop(cmd) {
+    if(test(cmd.x1) == "s" && test(cmd.x2) != "s") cmd.x1 = -50
+    if(test(cmd.x1) != "l" && test(cmd.x2) == "l") cmd.x2 = 50
+    
+    if(test(cmd.y1) == "s" && test(cmd.y2) != "s") cmd.y1 = -50
+    if(test(cmd.y1) != "l" && test(cmd.y2) == "l") cmd.y2 = 50
+
+    if(test(cmd.z1) == "s" && test(cmd.z2) != "s") cmd.z1 = -50
+    if(test(cmd.z1) != "l" && test(cmd.z2) == "l") cmd.z2 = 50
+    
+
+    if(cmd.x1 >= -50 && cmd.x1 <= 50 && cmd.y1 >= -50 && cmd.y1 <= 50 && cmd.z1 >= -50 && cmd.z1 <= 50) return cmd
+
+    else {
+        cmd.mode = "oob"
+        console.log(cmd)
+        return cmd
     }
 }
 
-function run(params) {
+function test(value){
+    let result = false
+    if((value / 50) > 1) result = "l"
+    else if((value / 50) < -1) result = "s"
+    return result 
+}
+
+function executeCommand(cmd) {
+    if(cmd.mode != "oob"){
+        for(let x = cmd.x1; x <= cmd.x2; x++){
+            for(let y = cmd.y1; y <= cmd.y2; y++){
+                for(let z = cmd.z1; z <= cmd.z2; z++){
+                    if(cmd.mode == "off"){
+                        if(activeCubes.delete(`${x}${y}${z}`)) o++
+                    } 
+                    else if(cmd.mode == "on"){
+                        activeCubes.add(`${x}${y}${z}`)                    
+                    }    
+                }
+            }
+        }
+    } 
+}
+
+function run() {
     
     console.time("run")
     for(i in lines){
-        executeCommand(makeCommand(lines[i])) 
-        if(i == 19) break
+        executeCommand(crop(makeCommand(lines[i]))) 
     }
     console.timeEnd("run")
     console.log("ON cubes:", activeCubes.size, `  Turned off cubes ${o} times`)
 }
 
 run()
+
+//console.log(crop(makeCommand(lines[21])))
+
